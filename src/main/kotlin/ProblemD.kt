0ed4dabbdc01f25f4af9cc1521e08aef15ed9bb3
Michaelo0 <nadnedmad@gmail.com>
2021-09-03 01:17:41 +0300

    Update ProblemD.kt

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -3,7 +3,12 @@ class ProblemD {
     fun main() {
 
         //ваша реализация в методе main
-
+        repeat(readLine()!!.toInt()) {
+            val n = readLine()!!.toInt()
+            val a = readLine()!!.split(" ").map { it.toLong() }
+            val answer = a.sortedDescending().withIndex().maxOf { (it.index + 1) * it.value }
+            println(answer)
+        }
     }
 
 }
