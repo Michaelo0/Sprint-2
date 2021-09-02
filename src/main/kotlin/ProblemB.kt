19d322e187b60113f59fd9c272e7df46792b29a0
Michaelo0 <nadnedmad@gmail.com>
2021-09-03 00:04:14 +0300

    Update ProblemB.kt

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -3,7 +3,17 @@ class ProblemB {
     fun main() {
 
         //ваша реализация в методе main
+        var t = readInt();
+        var arr = ArrayList<StringBuilder>()
+        for (i in 1..t){
+            val (n, k) = readLine()!!.split(' ').map(String::toInt)
+            var str = StringBuilder()
 
+            for (j in 1..n)
+                str.append(('a' + j % k).toChar())
+            arr.add(str)
+        }
+        arr.forEach{println(it)}
     }
 
 }
