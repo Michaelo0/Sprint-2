8f39066c2533fe7055941055a460d231ba47359f
Michaelo0 <nadnedmad@gmail.com>
2021-09-03 00:49:22 +0300

    Update ProblemC.kt

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -3,7 +3,16 @@ class ProblemC {
     fun main() {
 
         //ваша реализация в методе main
+        var n = readInt()
 
+        var list = ArrayList<Int>(readLine()!!.split(' ').map(String::toInt))
+
+        list.sort()
+        var sum = 0
+        for (i in 1 .. (list.size - 1) step 2){
+            sum += list.get(i) - list.get(i - 1)
+        }
+        println(sum)
     }
 
 }
