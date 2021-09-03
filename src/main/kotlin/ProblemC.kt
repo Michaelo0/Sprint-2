8b4783534a4a1eee8811e1d69a60059a4c2d3701
Michaelo0 <nadnedmad@gmail.com>
2021-09-03 18:15:13 +0300

    Update ProblemC.kt

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -3,14 +3,14 @@ class ProblemC {
     fun main() {
 
         //ваша реализация в методе main
-        var n = readInt()
+        val n = readInt()
 
-        var list = ArrayList<Int>(readLine()!!.split(' ').map(String::toInt))
+        val list = ArrayList<Int>(readLine()!!.split(' ').map(String::toInt))
 
         list.sort()
         var sum = 0
-        for (i in 1 .. (list.size - 1) step 2){
-            sum += list.get(i) - list.get(i - 1)
+        for (i in 1 until list.size step 2){
+            sum += list[i] - list[i - 1]
         }
         println(sum)
     }
